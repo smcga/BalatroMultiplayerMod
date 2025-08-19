77e9effb4e6a548e9b9b10c710b5fd35a52551ec
cg223 <cg223celeste@gmail.com>
2025-08-19 06:09:04 -0400

    update tmj compat for v4

--- compatibility/TooManyJokers.lua
+++ compatibility/TooManyJokers.lua
@@ -1,4 +1,9 @@
 if TMJ then
+    --TMJ v3
     TMJ.ALLOW_HIGHLIGHT = false --this is implemented in a hook, can't move it over here
     G.FUNCS.tmj_spawn = function() error("This is not allowed in Multiplayer") end
+
+    --TMJ v4
+    TMJ.FUNCS.CHEAT_TOGGLE = function() return end --this would otherwise return the toggle which allows below config to be changed
+    TMJ.config.disable_ctrl_enter = true 
 end
