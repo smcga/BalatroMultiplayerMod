bdb21a03bbef551c5557a0c1056fc35eb7412830
Stephen Kirk <me@thestephen.net>
2025-08-22 12:37:35 +0200

    hide bloodstone from collection

--- objects/jokers/bloodstone.lua
+++ objects/jokers/bloodstone.lua
@@ -10,7 +10,7 @@ SMODS.Joker({
 	rarity = 2,
 	cost = 7,
 	pos = { x = 0, y = 8 },
-	-- no_collection = true,
+	no_collection = true,
 	in_pool = function(self)
 		return MP.UTILS.is_standard_ruleset() and MP.LOBBY.code
 	end,
