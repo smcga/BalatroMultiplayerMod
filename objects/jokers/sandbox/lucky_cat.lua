d59732cb71a3111f82e51cc7e87bcb35b3dde77b
Stephen Kirk <me@thestephen.net>
2025-09-04 00:07:35 +0200

    remove confusing comment

--- objects/jokers/sandbox/lucky_cat.lua
+++ objects/jokers/sandbox/lucky_cat.lua
@@ -42,7 +42,7 @@ SMODS.Joker({
 			xmult = card.ability.extra.Xmult,
 		} end
 	end,
-	in_pool = function(self, args) --equivalent to `enhancement_gate = 'm_lucky'`
+	in_pool = function(self, args)
 		if not (MP.LOBBY.config.ruleset == "ruleset_mp_sandbox" and MP.LOBBY.code) then return false end
 		for _, playing_card in ipairs(G.playing_cards or {}) do
 			if SMODS.has_enhancement(playing_card, "m_lucky") then return true end
